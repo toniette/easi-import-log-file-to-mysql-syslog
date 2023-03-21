fb5fe5c561f5b315f6d26fc628aec1103bce8136
toniette <luis_ti@outlook.com>
2023-03-21 14:37:07 -0300

    - improving code readability

--- parser.py
+++ parser.py
@@ -2,23 +2,30 @@ import re
 import mysql.connector
 from datetime import datetime
 
+log_file = './auth.log'
+regex = r'^(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}) (\w+) (\w+)(\[\d+\])?: (.*)$'
+
 db_config = {
     'user': 'user',
     'password': 'password',
     'host': 'host',
     'database': 'database'
 }
-
-regex = r'^(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}) (\w+) (\w+)(\[\d+\])?: (.*)$'
 conn = mysql.connector.connect(**db_config)
 cursor = conn.cursor()
-log_file = './auth.log'
+
+def strip(value):
+    return str(value).strip()
 
 def get_parsed_date(date):
     source_date_format = '%Y %b %d %H:%M:%S'
     target_date_format = '%Y-%m-%d %H:%M:%S'
     default_year = '2022'
-    return datetime.strptime(default_year + ' ' + date, source_date_format).strftime(target_date_format)
+    full_date = default_year + ' ' + date
+    return strip(datetime.strptime(full_date, source_date_format).strftime(target_date_format))
+
+def get_parsed_sys_log_tag(appname, pid):
+    return strip(appname + (pid or ''))
 
 with open(log_file) as f:
     for line in f:
@@ -28,9 +35,9 @@ with open(log_file) as f:
                     "VALUES(%(ReceivedAt)s, %(FromHost)s, %(SysLogTag)s, %(Message)s)"
             params = {
                 'ReceivedAt': get_parsed_date(match.group(1)),
-                'FromHost': match.group(2),
-                'SysLogTag': match.group(3) + (match.group(4) or ''),
-                'Message': match.group(5)
+                'FromHost': strip(match.group(2)),
+                'SysLogTag': get_parsed_sys_log_tag(match.group(3), match.group(4)),
+                'Message': strip(match.group(5))
             }
             cursor.execute(query, params)
             conn.commit()

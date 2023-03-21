a2196c7b0e3fb42aaa430c9abb64f1087a6420f3
toniette <luis_ti@outlook.com>
2023-03-20 21:57:14 -0300

    - refactoring to use safe query params

--- parser.py
+++ parser.py
@@ -3,10 +3,10 @@ import mysql.connector
 from datetime import datetime
 
 db_config = {
-    'user': 'user',
+    'user': 'root',
     'password': 'password',
-    'host': 'host',
-    'database': 'database'
+    'host': '10.0.119.232',
+    'database': 'Syslog'
 }
 
 regex = r'^(\w{3} \d{1,2} \d{2}:\d{2}:\d{2}) (\w+) (\w+)(\[\d+\])?: (.*)$'
@@ -27,10 +27,18 @@ with open('./auth.log') as f:
             appcode = '' if match.group(4) is None else match.group(4)
             message = match.group(5)
 
-            query = f"INSERT INTO SystemEvents(ReceivedAt, FromHost, SysLogTag, Message) " \
-                    f"VALUES('{date_time}', '{hostname}', '{appname+appcode}', '{message}')"
+            query = "INSERT INTO SystemEvents(ReceivedAt, FromHost, SysLogTag, Message) " \
+                    "VALUES(%(date_time)s, %(hostname)s, %(app)s, %(message)s)"
 
-            cursor.execute(query)
+            cursor.execute(
+                query,
+                {
+                    'date_time': date_time,
+                    'hostname': hostname,
+                    'app': appname + appcode,
+                    'message': message
+                }
+            )
             conn.commit()
 cursor.close()
 conn.close()
